0504fc4cec62ba15ec5f73fc1f796600dd94c13d
CodeSandbox <support@codesandbox.io>
2024-06-04 14:34:08 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import type { ReactElement } from 'react';
+import { cloneElement } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+interface IHOC {
+  children: ReactElement & {ref: HTMLElement}
+}
 
+function HOC = ({children}: IHOC): ReactElement => {
+return cloneElement(children, {...children.props})
+}
+function ChildElement = (): ReactNode => {
+  return <p>I am a child element</p>
+}
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HOC>
+<ChildElement/>
+      </HOC>
   );
 }
 
